da03a4701fbeb773eb194148df90883371238876
bye1158 <9522159@gmail.com>
2025-09-18 08:42:45 +0800

    Update keep.sh

--- keep.sh
+++ keep.sh
@@ -8,7 +8,7 @@ US_ORG=""                # US组织名称
 SPACE="dev" # 多区请使用同样的空间名称
 
 # 要保活的URL列表,多个用英文空格分隔
-URLS="https://xxxx.cfapps.ap21.hana.ondemand.com https://xxxx.cfapps.us10-001.hana.ondemand.com"  
+URLS="https://sgzdmbgc.cfapps.ap21.hana.ondemand.com https://xxxx.cfapps.us10-001.hana.ondemand.com"  
 
 # 颜色定义
 green() {
